bb371d3d43aaede41dc6c23ecbba3aaf98c523f6
sijanr <sijan1.rijal@gmail.com>
2021-09-24 13:14:37 -0700

    Check the number of arguments

--- redact.py
+++ redact.py
@@ -12,17 +12,20 @@ def getJson(url, path):
     return json_data
 
 def main():
-    # Work with foaas
-    foaas_url = 'foaas.com'
-    json_data = getJson(foaas_url, sys.argv[1])
-    subtitle = json_data["subtitle"]
+    if len(sys.argv) > 1:
+        # Work with foaas
+        foaas_url = 'foaas.com'
+        json_data = getJson(foaas_url, sys.argv[1])
+        subtitle = json_data["subtitle"]
 
-    #Profanity filter
-    pm_url = 'www.purgomalum.com'
-    pm_quoted_url = urllib.parse.quote(json_data['message'].strip())
-    json_data = getJson(pm_url, "/service/json?text=" + pm_quoted_url)
-    dict = {"message": json_data["result"], "subtitle": subtitle}
-    print(json.dumps(dict, indent = 3))
+        #Profanity filter
+        pm_url = 'www.purgomalum.com'
+        pm_quoted_url = urllib.parse.quote(json_data['message'].strip())
+        json_data = getJson(pm_url, "/service/json?text=" + pm_quoted_url)
+        dict = {"message": json_data["result"], "subtitle": subtitle}
+        print(json.dumps(dict, indent = 3))
+    else:
+        print("Usage: redact URL")
 
 
 if __name__ == '__main__':
